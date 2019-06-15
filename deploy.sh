c2aa699b5ef17e2e6f8aa801f52e9dcce8188479
Steve Johnson <stephenjohnsonm@gmail.com>
2019-06-15 21:27:17 +0900

    Add app engine deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+echo "runtime: go111
+instance_class: F1
+env_variables:
+  SIGNING_SECRET: '$SIGNING_SECRET'
+  BOT_TOKEN: '$BOT_TOKEN'" > app.yaml
+
+echo "successfully created app.yaml, deploying app"
+
+gcloud app deploy app.yaml
+
+echo "deploy finished, deleting app.yaml"
+
+rm app.yaml
