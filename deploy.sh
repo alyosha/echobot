53720af6e1940000205d7a0d6349b72e8424dda7
Steve Johnson <stephenjohnsonm@gmail.com>
2019-06-18 02:52:12 +0900

    Add new env variables to deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ instance_class: F1
 env_variables:
   SIGNING_SECRET: '$SIGNING_SECRET'
   BOT_TOKEN: '$BOT_TOKEN'
+  CACHE_DEFAULT_EXPIRATION: '$CACHE_DEFAULT_EXPIRATION'
+  CACHE_CLEANUP_INTERVAL: '$CACHE_CLEANUP_INTERVAL'
   BOT_ID: '$BOT_ID'" > app.yaml
 
 echo "successfully created app.yaml, deploying app"
